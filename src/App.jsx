213f589595fc5043b0445183c73e153056626839
Francois Roland <froland@froland.be>
2023-06-05 16:42:38 +0200

    Check todo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import './App.css'
 import TodoForm from "./TodoForm.jsx";
 import {useState} from "react";
+import useSWR from "swr";
+
+function TodoItem({item}) {
+  function handleClick(item) {
+    item.done = !item.done;
+    fetch(`/api/todos/${item.id}`, {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(item)
+    });
+  }
+
+  return <li key={item.id}>
+    <input type="checkbox" defaultChecked={item.done} onClick={() => handleClick(item)}/>
+    {item.description}
+  </li>;
+}
 
 function TodoList() {
-  return (<h2>Todo list</h2>);
+  const fetcher = url => fetch(url).then(r => r.json());
+  const {data, isLoading, error} = useSWR('/api/todos', fetcher);
+  if (isLoading) return <p>Chargement...</p>
+  if (error) return <p>Erreur</p>
+  else return (
+    <main>
+      <h2>Todo list</h2>
+      <ul>
+        {data.map(todo => <TodoItem item={todo}/>)}
+      </ul>
+    </main>
+  );
 }
 
 function App() {
