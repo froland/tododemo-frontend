f54357af230d261d22435da83cd1d4c39db01fb4
Francois Roland <froland@froland.be>
2023-06-12 15:06:22 +0200

    Configure auth0 integration.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </Auth0Provider>,
 );
