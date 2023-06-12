56a0b7b197bdf01f1ab2ae074c73e623c0431ffd
Francois Roland <froland@froland.be>
2023-06-12 18:28:16 +0200

    Authenticate user.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { useState } from 'react';
 import './App.css';
+import LoginButton from './LoginButton.jsx';
+import LogoutButton from './LogoutButton.jsx';
 import TodoForm from './TodoForm.jsx';
 import TodoList from './TodoList.jsx';
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>En chargement...</div>;
+  }
 
   return (
     <>
-      <h1>Todo demo</h1>
-      {showForm ? <TodoForm onExit={() => setShowForm(false)}/> : <TodoList onShowForm={() => setShowForm(true)}/>}
+      <div>
+        <h1>Todo demo</h1>
+        {isAuthenticated ?
+          <div>
+            <p>Bonjour {user.name} <LogoutButton/></p>
+            {showForm ? <TodoForm onExit={() => setShowForm(false)}/> :
+              <TodoList onShowForm={() => setShowForm(true)}/>}
+          </div>
+          :
+          <LoginButton/>
+        }
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="teacher-projint-2223.eu.auth0.com"
+    clientId="mNHQkQomEzxKPSpn9JUdhK3LNaKPJK0G"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </Auth0Provider>,
 );
