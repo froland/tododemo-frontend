3c3376504d9bb4931d47f0fcb17a7e913af175d7
Francois Roland <froland@froland.be>
2023-06-12 16:24:28 +0200

    Add token to API requests.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { useState } from 'react';
 import './App.css';
+import LoginButton from './LoginButton.jsx';
+import LogoutButton from './LogoutButton.jsx';
 import TodoForm from './TodoForm.jsx';
 import TodoList from './TodoList.jsx';
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const { isLoading, isAuthenticated, user } = useAuth0();
+
+  if (isLoading) return <p>Chargement...</p>;
 
   return (
     <>
-      <h1>Todo demo</h1>
-      {showForm ? <TodoForm onExit={() => setShowForm(false)}/> : <TodoList onShowForm={() => setShowForm(true)}/>}
+      <div>
+        <h1>Todo demo</h1>
+        {isAuthenticated ? <>
+          <p>Bonjour {user.name} <LogoutButton/></p>
+          { showForm ? <TodoForm onExit={() => setShowForm(false)}/> : <TodoList onShowForm={() => setShowForm(true)}/> }
+        </>: <LoginButton/>}
+      </div>
     </>
   );
 }
